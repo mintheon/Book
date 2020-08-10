c4949bd914f50729903ae568ba76f018cc1566bc
min <shsh0491@gmail.com>
2020-08-11 00:02:42 +0900

    :pencil: 3,4장 정리 추가

--- 테스트 주도 개발/SampleSource/src/main/java/Dollar.java
+++ 테스트 주도 개발/SampleSource/src/main/java/Dollar.java
@@ -1,12 +1,17 @@
 public class Dollar {
-    int amount;
+    private int amount;
 
     public Dollar(int amount) {
         this.amount = amount;
     }
 
     Dollar times(int multiplier) {
-
         return new Dollar(amount * multiplier);
     }
+
+    @Override
+    public boolean equals(Object object) {
+        Dollar dollar = (Dollar) object;
+        return amount == dollar.amount;
+    };
 }
--- 테스트 주도 개발/SampleSource/src/test/java/TestTdd.java
+++ 테스트 주도 개발/SampleSource/src/test/java/TestTdd.java
@@ -1,15 +1,20 @@
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class TestTdd {
 
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        Dollar product = five.times(2);
-        assertEquals(10, product.amount);
-        product = five.times(3);
-        assertEquals(15, product.amount);
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
+    }
+
+    @Test
+    public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+//        추가
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
 }
