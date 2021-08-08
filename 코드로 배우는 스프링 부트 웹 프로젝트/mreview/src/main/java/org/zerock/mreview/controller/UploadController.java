09c873bb2f26d33f9e4874c14fe116f9884ec431
mintheon <mintheon@mintheon.com>
2021-08-08 23:43:29 +0900

    :pencil: [코드로 배우는 스프링부트 웹 프로젝트] chapter 8

---
+++ 코드로 배우는 스프링 부트 웹 프로젝트/mreview/src/main/java/org/zerock/mreview/controller/UploadController.java
@@ -0,0 +1,113 @@
+package org.zerock.mreview.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.zerock.mreview.dto.UploadResultDTO;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@Log4j2
+public class UploadController {
+
+    @Value("${org.zerock.upload.path}")
+    private String uploadPath;
+
+    @GetMapping("/display")
+    public ResponseEntity<byte[]> getFile(String fileName) {
+        ResponseEntity<byte[]> result = null;
+
+        try {
+            String srcFileName = URLDecoder.decode(fileName, "UTF-8");
+
+            log.info("fileName: " + srcFileName);
+
+            File file = new File(uploadPath + File.separator + srcFileName);
+
+            log.info("file: " + file);
+
+            HttpHeaders header = new HttpHeaders();
+
+            //MIME 타입 정리
+            header.add("Content-Type", Files.probeContentType(file.toPath()));
+
+            //파일 데이터 처리
+            result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return result;
+    }
+
+    @PostMapping("/uploadAjax")
+    public ResponseEntity<List<UploadResultDTO>> uploadFile(MultipartFile[] uploadFiles) {
+
+        for(MultipartFile uploadFile: uploadFiles) {
+            //이미지 파일만 업로드 가능
+            if(uploadFile.getContentType().startsWith("image") == false) {
+                log.warn("this file is not image type");
+                return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+            }
+
+            //실제 파일 이름 IE나 Edge는 전체 경로가 들어오므로
+            String originalName = uploadFile.getOriginalFilename();
+            String fileName = originalName.substring(originalName.lastIndexOf("\\") + 1);
+
+            log.info("fileName:" + fileName);
+
+            //날짜 폴더 생성
+            String folderPath = makeFolder();
+
+            //UUID
+            String uuid = UUID.randomUUID().toString();
+
+            String saveName = uploadPath + File.separator + folderPath + File.separator + uuid + "_" + fileName;
+
+            Path savePath = Paths.get(saveName);
+
+            try {
+                uploadFile.transferTo(savePath);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    private String makeFolder() {
+
+        String str = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+
+        String folderPath = str.replace("/", File.separator);
+
+        File uploadPathFolder = new File(uploadPath, folderPath);
+
+        if(uploadPathFolder.exists() == false) {
+            uploadPathFolder.mkdirs();
+        }
+
+        return folderPath;
+    }
+}
---
+++ 코드로 배우는 스프링 부트 웹 프로젝트/mreview/src/main/java/org/zerock/mreview/dto/UploadResultDTO.java
@@ -0,0 +1,26 @@
+package org.zerock.mreview.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+@Data
+@AllArgsConstructor
+public class UploadResultDTO implements Serializable {
+    private String fileName;
+    private String uuid;
+    private String folderPath;
+
+    public String getImageURL() {
+        try {
+            return URLEncoder.encode(folderPath + "/" + uuid + "_" + fileName, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        return "";
+    }
+}
